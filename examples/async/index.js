041141925cb530319e6ad984bee85c21a9c2908f
Dan Abramov <dan.abramov@gmail.com>
2015-08-11 14:59:10 +0300

    Fix

--- examples/async/index.js
+++ examples/async/index.js
@@ -1,7 +1,7 @@
+import 'babel-core/polyfill';
 import React from 'react';
 import Root from './containers/Root';
 import BrowserHistory from 'react-router/lib/BrowserHistory';
-import 'babel-core/polyfill';
 
 React.render(
   <Root history={new BrowserHistory()} />,
